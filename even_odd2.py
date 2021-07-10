76d77e1895ae0018a37f183bd2484b5328e37167
manasbhargava <manasbhargava1989@gmail.com>
2021-07-10 19:59:31 -0400

    Initial Commit

---
+++ even_odd2.py
@@ -0,0 +1,8 @@
+class Even_odd:
+    def __init__(self,x):
+        self.x=x
+    def even_or_odd(self):
+        if self.x % 2 == 0:
+            print("x is even")
+        else
+            print("x is odd")
